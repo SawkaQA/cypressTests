ac5512e65e16af97c158b30428b4d7e6c5a8b2b3
SawkaQA <aleksandr.sdobnov.qa@gmail.com>
2023-06-25 23:32:30 +0300

    add: cy.api

--- Cypress Jenkins/cypress/e2e/1-getting-started/apitest.cy.js
+++ Cypress Jenkins/cypress/e2e/1-getting-started/apitest.cy.js
@@ -1,45 +1,44 @@
 /// <reference types="cypress" />
 
-const BASE_URL = 'https://reqres.in/api';
-const getListUsers = () => cy.api(`${BASE_URL}/users`)
-const postCreate = () => 
+const BASE_URL = "https://reqres.in/api";
+const token = "asdsadasdasd";
+const expires = "21.21.121.23";
+const getListUsers = () => cy.api(`${BASE_URL}/users`);
+const postCreate = () =>
     cy.api({
-        method: 'POST',
+        method: "POST",
         url: `${BASE_URL}/users`,
         body: {
-            "name": "morpheus",
-            "job": "leader"
-        }
-    })
+            name: "morpheus",
+            job: "leader",
+        },
+    });
 
-describe('API Test', () => {
-   it('Get users verify status code', () => {
-     getListUsers()
-     .then(response => {
-        console.log(response);
-     })
-      .its('status')
-      .should('be.eq', 200)
-   })
+describe("API Test", () => {
+    it("Get users verify status code", () => {
+        getListUsers()
+            .then((response) => {
+                console.log(response);
+            })
+            .its("status")
+            .should("be.eq", 200);
+    });
 
-   it('Get users verify body', () => {
-    getListUsers()
-     .its('body')
-     .then(responseBody =>{
-        console.log("responseBody = ", responseBody);
-        expect(responseBody).to.have.any.keys('data')
-     })
-  })
+    it("Get users verify body", () => {
+        getListUsers().its("body");
+    });
 
-  it('Post users - verify status code', () => {
-    postCreate()
-    .its('status')
-    .should('be.eq', 201)
-  })
+    it("Post users - verify status code", () => {
+        postCreate().its("status").should("be.eq", 201);
+    });
 
-  it('Post users - verify respone has name be a string', () => {
-    postCreate()
-    .its('body.name')
-    .should('be.a', 'string')
-  })
-})
+    it("Post users - verify respone has name be a string", () => {
+        postCreate().its("body.name").should("be.a", "string");
+    });
+
+    it("Login to the site", () => {
+        cy.setCookie("userName", "Sawka");
+        cy.setCookie("token", token);
+        cy.setCookie("expires", expires);
+    });
+});
